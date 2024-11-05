3c5f98ecb13c7f35d21c29072a6c36f9d115a731
github_Venky789-del <github venkateswaramadhav@gmail.com>
2024-11-05 15:08:35 +0530

    my files

---
+++ java/dj.java
@@ -0,0 +1,8 @@
+clsaa MainDdemo{
+    public static viod main (String arg[]){
+        int[] numbers={1,4,5,8,7}
+        for(int i=0; i<numbers,length;i++){
+            System.out.println(age[i]);
+        }
+    }
+}
---
+++ java/venky1.java
@@ -0,0 +1,8 @@
+class MainDemo{
+    public static viod main (String arg[]){
+        int[] numbers={1,4,5,8,7}
+        for(int i=0; i<numbers,length;i++){
+            System.out.println(age[i]);
+        }
+    }
+}
